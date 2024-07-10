92330d88ef845893cd8ce8c48a25327c861ad2f5
Mikle Jaxon <121095868+MikleJaxon@users.noreply.github.com>
2024-07-11 02:25:43 +0300

    Update settings.py

--- bike_rental/bike_rental/settings.py
+++ bike_rental/bike_rental/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)23x^^g@a8sj#rws=8-6my)f3u8*q4b++^il#z7)e^a37ev@!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '172.21.0.2', '80.85.143.62']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '172.21.0.2', '80.85.143.62', 'zaryazheniy.xyz', 'www.zaryazheniy.xyz']
 
 
 # Application definition
